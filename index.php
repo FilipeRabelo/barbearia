2e5f67106f8bbba0194183840449b45ff71fa74f
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-08-06 14:57:51 -0300

    Barbearia projeto

--- index.php
+++ index.php
@@ -1,1 +1,41 @@
-site, va para <a href="http://localhost/barbearia/sistema/">SISTEMA</a>
+<!DOCTYPE html>
+<html lang="PT-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site Barbearia</title>
+    <style>
+        body {
+            background-image: url('fundoLoginCelular.jpg');
+        }
+
+        h1{
+            color: red;
+            font-weight: bold;
+            text-align: center;
+            justify-content: center;
+            padding:0 100px 0 100px;
+        }
+
+        a{
+            color: white;
+            text-decoration: none;            
+        }
+
+        a:hover{
+            color: red;
+            border: 1px solid red;
+        }
+    </style>
+</head>
+
+<body >
+
+    <h1>Barbearia Pelo Longo</h1>
+    <h1>Site, Vá para o <a href="http://localhost/barbearia/sistema/">SISTEMA</a></h1>
+
+</body>
+
+</html>
